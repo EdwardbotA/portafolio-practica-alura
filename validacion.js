31e48e2fe20ec49f1ffb6243cb69bcdf14b342e7
Edward B <ebotelloaroca00@gmail.com>
2024-04-07 19:25:18 -0500

    agregar verificacion de nombre en JS

--- validacion.js
+++ validacion.js
@@ -1,1 +1,33 @@
-//Haz tú validación en javascript acá
+const inputName = document.querySelector('#nombre')
+const labelName = document.querySelector('label[for="nombre"]')
+
+function isEmpty(element) {
+  if (!element.value) {
+    const error = document.createElement('p')
+    error.classList.add('error')
+    error.textContent = 'agrege su nombre'
+    labelName.insertAdjacentElement('afterend', error)
+    return true
+  }
+
+  return false
+}
+
+inputName.addEventListener('input', () => {
+  if (parseInt(inputName.attributes.maxlength.value) === inputName.value.length) {
+    const error = document.createElement('p')
+    error.classList.add('error')
+    error.textContent = 'longitud maxima'
+    
+    labelName.style.color = 'red'
+    inputName.style.borderBottom = '2px solid red'
+    labelName.insertAdjacentElement('afterend', error)
+  } else {
+    const error = document.querySelector('.error')
+    error.remove()
+    labelName.style.color = 'var(--cor-de-btn)'
+    inputName.style.borderBottom = '2px solid var(--cor-de-btn)'
+  }
+})
+
+let vacio = isEmpty(inputName)
