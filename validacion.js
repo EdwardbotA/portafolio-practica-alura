a42e80b5c43528e57e62ce0ce906ac1fe652e989
Edward B <ebotelloaroca00@gmail.com>
2024-04-07 20:37:09 -0500

    arreglar bug de color en el imput al no tener el focus

--- validacion.js
+++ validacion.js
@@ -24,9 +24,23 @@ inputName.addEventListener('input', () => {
     labelName.insertAdjacentElement('afterend', error)
   } else {
     const error = document.querySelector('.error')
-    error.remove()
-    labelName.style.color = 'var(--cor-de-btn)'
-    inputName.style.borderBottom = '2px solid var(--cor-de-btn)'
+    if (error) {
+      error.remove()
+      labelName.style.color = 'var(--cor-de-btn)'
+      inputName.style.borderBottom = '2px solid var(--cor-de-btn)'
+      inputName.addEventListener('focus', function() {
+        // Cuando el input tiene focus, agrega los estilos deseados
+        labelName.style.color = 'var(--cor-de-btn)'
+        inputName.style.borderBottom = '2px solid var(--cor-de-btn)'
+      });
+      
+      // Agrega otro event listener para el evento 'blur'
+      inputName.addEventListener('blur', function() {
+        // Cuando el input pierde el focus, remueve los estilos
+        labelName.style.color = '#4b4a4ad9'
+        inputName.style.borderBottom = '2px solid #4b4a4ad9'
+      });
+    }
   }
 })
 
